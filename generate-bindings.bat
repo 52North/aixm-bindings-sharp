9e28a6d2afc4c6a9a613d0326dcafd7c6dd76504
matthesrieke <m.rieke@52north.org>
2014-01-24 11:21:07 +0100

    include required schemas

--- generate-bindings.bat
+++ generate-bindings.bat
@@ -1,3 +1,3 @@
 REM this compiles everything into one big .cs file
 
-xsd.exe ..\..\gml\3.2.1\gml.xsd ..\..\xlink\1.0.0\xlinks.xsd ..\..\iso\19139\20070417\gmd\gmd.xsd ..\..\iso\19139\20070417\gco\gco.xsd ..\..\iso\19139\20070417\gss\gss.xsd ..\..\iso\19139\20070417\gts\gts.xsd ..\..\iso\19139\20070417\gsr\gsr.xsd AIXM_Features.xsd /classes /namespace:aero.aixm.v51
+xsd.exe xsd\gml\3.2.1\gml.xsd xsd\xlink\1.0.0\xlinks.xsd xsd\iso\19139\20070417\gmd\gmd.xsd xsd\iso\19139\20070417\gco\gco.xsd xsd\iso\19139\20070417\gss\gss.xsd xsd\iso\19139\20070417\gts\gts.xsd xsd\iso\19139\20070417\gsr\gsr.xsd xsd\aixm\5.1\AIXM_Features.xsd /classes /namespace:aero.aixm.v51
