dda61b957eee7e06175025e79132adb372dc5a92
matthesrieke <m.rieke@52north.org>
2014-01-24 13:56:57 +0100

    adjust to xlink simpleAttrs

--- generate-bindings.bat
+++ generate-bindings.bat
@@ -1,3 +1,3 @@
 REM this compiles everything into one big .cs file
 
-xsd.exe xsd\gml\3.2.1\gml.xsd xsd\xlink\1.0.0\xlinks.xsd xsd\iso\19139\20070417\gmd\gmd.xsd xsd\iso\19139\20070417\gco\gco.xsd xsd\iso\19139\20070417\gss\gss.xsd xsd\iso\19139\20070417\gts\gts.xsd xsd\iso\19139\20070417\gsr\gsr.xsd xsd\aixm\5.1\AIXM_Features.xsd /classes /namespace:aero.aixm.v51
+xsd.exe xsd\gml\3.2.1\gml.xsd xsd\xlink\1.0.0\xlink.xsd xsd\iso\19139\20070417\gmd\gmd.xsd xsd\iso\19139\20070417\gco\gco.xsd xsd\iso\19139\20070417\gss\gss.xsd xsd\iso\19139\20070417\gts\gts.xsd xsd\iso\19139\20070417\gsr\gsr.xsd xsd\aixm\5.1\AIXM_Features.xsd /classes /namespace:aero.aixm.v51
